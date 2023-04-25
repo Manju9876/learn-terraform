26d0012df27dcb59ac46e40cfd6404dcf3bfd6f2
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-25 22:11:08 +0530

    source common code

--- example/output.tf
+++ example/output.tf
@@ -1,13 +1,19 @@
-output "sample_string" {
-  value = var.sample_string
+#output "sample_string" {
+#  value = var.sample_string
+#}
+#
+####If variable is a combination of some strings then following is the syntax. ${} is mandatory if we include the variable inside a string
+#
+#output "sample_one" {
+#  value = "value of sample_string= ${var.sample_string}"
+#}
+variable "sample_list" {
+  default = [
+  100,
+  "asakjkhf",
+  true
+  ]
 }
-
-###If variable is a combination of some strings then following is the syntax. ${} is mandatory if we include the variable inside a string
-
-output "sample_one" {
-  value = "value of sample_string= ${var.sample_string}"
-}
-
-output "sample_list_2ndvalue" {
+output "sample_list_2" {
   value = var.sample_list[2]
 }
