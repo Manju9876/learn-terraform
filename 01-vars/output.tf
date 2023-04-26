183002bbd06dbfaf0aacaf95aed059b7afc9259f
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-26 17:53:29 +0530

    terraform -var-file

--- 01-vars/output.tf
+++ 01-vars/output.tf
@@ -1,14 +1,14 @@
 #prints the list 0f values in a single variable
 
 output "sample_list_2" {
-  value = [var.sample_list[1], var.sample_list[2]]
+  value = var.sample_list[1]
 }
 
 #this is dictnory type variable
 # prints values with the name specified to it
 
 output "sample_dict_strings" {
-  value = [var.sample_dict["string1"], var.sample_dict["string2"]]
+  value = var.sample_dict["string1"]
 }
 output "env" {
   value = var.env
