f17eb7101fbec33e068faae9c5ccc483c258fcc2
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-02 15:35:20 +0530

    loops

--- 03-loops/for-each/main-for-each.tf
+++ 03-loops/for-each/main-for-each.tf
@@ -1,11 +1,10 @@
+
 resource "null_resource" "fruits" {
-  count = 10
-}
-resource "null_resource" "null1" {
+
   count = length(var.fruits)
 
   provisioner "local-exec" {
-    command = "echo fruit name is - $(each.key)"
+    command =  "echo Fruit Name - ${each.key}"
   }
 }
 
