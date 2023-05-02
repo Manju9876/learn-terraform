561cdb9a35c92f1edafd972ed7efc104d67d4714
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-02 20:01:31 +0530

    d

--- 03-loops/for-each/main-for-each.tf
+++ 03-loops/for-each/main-for-each.tf
@@ -1,54 +1,54 @@
-#
-#resource "null_resource" "fruits" {
-#
-# for_each = var.fruits
-#
-#  provisioner "local-exec" {
-#    command =  "echo Fruit Name - ${each.key} - ${each.value}"
-#  }
-#}
-#
-#
-#resource "null_resource" "fruits1" {
-#
-#  for_each = var.fruits1
-#
-#  provisioner "local-exec" {
-#    command =  "echo Fruit Name - ${each.key} - ${each.value["count"]}"
-#  }
-#}
-#
-#variable "fruits1" {
-#  default = {
-#    apple = {
-#      name = "apple"
-#      count = 10
-#    }
-#    mango = {
-#      name = "mango"
-#      count = 5
-#    }
-#    oranges = {
-#      name = "oranges"
-#      count = 10
-#    }
-#  }
-#}
-#
-#variable "fruits" {
-#  default = {
-#    apple = 10
-#    banana = 20
-#    oranges = 15
-#  }
-#}
+
+resource "null_resource" "fruits" {
+
+ for_each = var.fruits
+
+  provisioner "local-exec" {
+    command =  "echo Fruit Name - ${each.key} - ${each.value}"
+  }
+}
+
+
+resource "null_resource" "fruits1" {
+
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command =  "echo Fruit Name - ${each.key} - ${each.value["count"]}"
+  }
+}
+
+variable "fruits1" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 10
+    }
+    mango = {
+      name = "mango"
+      count = 5
+    }
+    oranges = {
+      name = "oranges"
+      count = 10
+    }
+  }
+}
+
+variable "fruits" {
+  default = {
+    apple = 10
+    banana = 20
+    oranges = 15
+  }
+}
 
 
 resource "null_resource" "person_name" {
-  count = 10
+  count = 3
 
   provisioner "local-exec" {
-    command = "echo persons name  - ${var.persons[count.index % length(var.persons)]}"
+    command = "echo persons name  - ${var.persons[count.index]}
   }
 }
 
