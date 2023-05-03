3c60d47b1e85b7ea17ff187a3c9fdd08f7aad276
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-03 16:10:54 +0530

    f

--- 04-functions/sample.tf
+++ 04-functions/sample.tf
@@ -5,5 +5,27 @@ variable "convert" {
 }
 
 output "conversion" {
-  value = var.convert
+  value = upper(var.convert)
+}
+
+## printing only the first value of the strng
+
+variable "sample" {
+  default = [ "abcd", "efgh" ]
+}
+output "sample" {
+  value = element(var.sample,0)
+}
+
+# printing value using lookup function
+
+variable "sample1" {
+  default = {
+    abcd = 1000
+    efgh = 2000
+  }
+}
+
+output "sample1" {
+  value = lookup(var.sample1, "a1", "no value found" )
 }
