3d5c9aafa2af16331e044f06805180a1ae310dcb
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-04 19:07:28 +0530

    sdf

---
+++ 04-functions/05-module/module/main.tf
@@ -0,0 +1,20 @@
+resource "null_resource" "nothing" {
+  provisioner "local-exec" {
+    command = "echo hello world form moduel"
+  }
+}
+resource "null_resource" "nothing1" {
+  provisioner "local-exec" {
+    command = "echo hello world form moduel"
+  }
+}
+resource "null_resource" "nothing2" {
+  provisioner "local-exec" {
+    command = "echo hello world form moduel"
+  }
+}
+resource "null_resource" "nothing3" {
+  provisioner "local-exec" {
+    command = "echo hello world form moduel"
+  }
+}
