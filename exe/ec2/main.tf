1b3cb1b622e528ada6fe04c81d6b7486a880d76a
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-05-04 20:01:54 +0530

    asf

---
+++ exe/ec2/main.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
+
+
+ resource "aws_instance" "instance_name" {
+
+   ami                    = data.aws_ami.centos.image_id
+   instance_type          = "t3.micro"
+   vpc_security_group_ids = var.sg_id
+
+
+   tags = {
+     Name = test
+   }
+}
+
+variable "sg_id" {}
